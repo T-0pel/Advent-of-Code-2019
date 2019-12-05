31efc8d9f97f85b198861ec0ffe6a8b73fdac244
Tomas Oplatek <oplatek.t@gmail.com>
2019-12-05 18:31:43 +0000

    Add Day1

---
+++ AdventOfCode2019/Day1/Program.cs
@@ -0,0 +1,72 @@
+﻿using SharedLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day1
+{
+    internal class Program
+    {
+        private const string DirectoryName = "Day1";
+
+        private static void Main(string[] args)
+        {
+            var lines = FileHelper.GetLines(DirectoryName);
+
+            var masses = lines.Select(int.Parse).ToList();
+
+            Console.WriteLine(CalculateFuel(masses));
+
+            var totalFuel = 0;
+            foreach (var mass in masses)
+            {
+                totalFuel += CalculateFuelRecursively(mass, 0);
+            }
+
+            Console.WriteLine(totalFuel);
+        }
+
+        private static int CalculateFuel(List<int> masses)
+        {
+            var totalFuel = 0;
+
+            foreach (var mass in masses)
+            {
+                var fuel = mass / 3 - 2;
+                totalFuel += fuel;
+            }
+
+            return totalFuel;
+        }
+
+        private static int CalculateFuelRecursively(int mass, int fuelCost)
+        {
+            var fuel = mass / 3 - 2;
+            if (fuel <= 0)
+            {
+                return fuelCost;
+            }
+            else
+            {
+                return CalculateFuelRecursively(fuel, fuelCost + fuel);
+            }
+        }
+
+        private static int CalculateFuelWithCycle(int mass, int fuelCost)
+        {
+            while (true)
+            {
+                var fuel = mass / 3 - 2;
+                if (fuel <= 0)
+                {
+                    return fuelCost;
+                }
+                else
+                {
+                    mass = fuel;
+                    fuelCost += fuel;
+                }
+            }
+        }
+    }
+}
